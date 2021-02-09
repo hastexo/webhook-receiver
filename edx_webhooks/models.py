2e40e1ca36e745b5551131358d8dcabd0af54776
Florian Haas <florian@citynetwork.eu>
2021-02-09 14:38:28 +0100

    Refactor models
    Define Order and OrderItem on the edx_webhooks level as abstract objects,
    and then have edx_shopify and edx_webhooks subclass them.

--- edx_webhooks/models.py
+++ edx_webhooks/models.py
@@ -1,5 +1,6 @@
 from django.db.models import Model
 from django.db.models import GenericIPAddressField, BinaryField, DateTimeField
+from django.db.models import CharField, BigIntegerField, EmailField
 try:
     # Django 3.1 and later has a built-in JSONField
     from django.db.models import JSONField
@@ -77,3 +78,88 @@ class JSONWebhookData(WebhookData):
     # In addition to the webhook source and timestamp, we also want
     # the webhook content, which in this case is always JSON data.
     content = JSONField(null=True)
+
+
+class Order(ConcurrentTransitionMixin, Model):
+    class Meta:
+        app_label = APP_LABEL
+        abstract = True
+
+    NEW = STATE.NEW
+    PROCESSING = STATE.PROCESSING
+    PROCESSED = STATE.PROCESSED
+    ERROR = STATE.ERROR
+
+    CHOICES = STATE.CHOICES
+
+    id = BigIntegerField(primary_key=True, editable=False)
+    email = EmailField()
+    first_name = CharField(max_length=254)
+    last_name = CharField(max_length=254)
+    received = DateTimeField(default=timezone.now)
+    status = FSMIntegerField(choices=CHOICES,
+                             default=NEW,
+                             protected=True)
+
+    @transition(field=status,
+                source=NEW,
+                target=PROCESSING,
+                on_error=ERROR)
+    def start_processing(self):
+        logger.debug('Processing order %s' % self.id)
+
+    @transition(field=status,
+                source=PROCESSING,
+                target=PROCESSED,
+                on_error=ERROR)
+    def finish_processing(self):
+        logger.debug('Finishing order %s' % self.id)
+
+    @transition(field=status,
+                source=PROCESSING,
+                target=ERROR)
+    def fail(self):
+        logger.debug('Failed to process order %s' % self.id)
+
+
+class OrderItem(ConcurrentTransitionMixin, Model):
+    class Meta:
+        app_label = APP_LABEL
+        abstract = True
+
+    NEW = STATE.NEW
+    PROCESSING = STATE.PROCESSING
+    PROCESSED = STATE.PROCESSED
+    ERROR = STATE.ERROR
+
+    CHOICES = STATE.CHOICES
+
+    sku = CharField(max_length=254)
+    email = EmailField()
+    status = FSMIntegerField(choices=CHOICES,
+                             default=NEW,
+                             protected=True)
+
+    @transition(field=status,
+                source=NEW,
+                target=PROCESSING,
+                on_error=ERROR)
+    def start_processing(self):
+        logger.debug('Processing item %s for order %s' % (self.id,
+                                                          self.order.id))
+
+    @transition(field=status,
+                source=PROCESSING,
+                target=PROCESSED,
+                on_error=ERROR)
+    def finish_processing(self):
+        logger.debug('Finishing item %s for order %s' % (self.id,
+                                                         self.order.id))
+
+    @transition(field=status,
+                source=PROCESSING,
+                target=ERROR)
+    def fail(self):
+        logger.debug('Failed to process item %s '
+                     'for order %s' % (self.id,
+                                       self.order.id))
