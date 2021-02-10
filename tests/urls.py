61cc18a858e4bb6d0f29a0226b1e873b8b8d2f2d
Florian Haas <florian@citynetwork.eu>
2021-02-10 12:12:43 +0100

    Remove tests/urls.py
    Now that we have edx_webhooks.urls and use that as ROOT_URLCONF in our
    base settings module, there is no longer any need for a separate URLs
    definition for tests.

--- tests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('webhooks/shopify/', include('edx_shopify.urls')),
-    path('webhooks/woocommerce/', include('edx_woocommerce.urls')),
-    path('admin/', admin.site.urls),
-]
