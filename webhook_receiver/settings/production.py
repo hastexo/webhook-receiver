1e047bf4554847b8bb4360e7d743236bf3d50206
Florian Haas <florian@citynetwork.eu>
2022-01-27 21:06:36 +0100

    Only populate overrides if set
    In settings.production, don't blindly override the values in the
    DATABASES['default'] dictionary; rather, override only those values
    that we actually find an environment variable for.

--- webhook_receiver/settings/production.py
+++ webhook_receiver/settings/production.py
@@ -28,23 +28,18 @@ if CONFIG_FILE:
 # The "make migrate" ("manage.py migrate") functionality needs to
 # honour the DB_MIGRATION_ environment variables.
 DB_OVERRIDES = dict(
-    PASSWORD=env.str('DB_MIGRATION_PASS',  # noqa: F405
-                     default=DATABASES['default']['PASSWORD']),  # noqa: F405
-    ENGINE=env.str('DB_MIGRATION_ENGINE',  # noqa: F405
-                   default=DATABASES['default']['ENGINE']),  # noqa: F405
-    USER=env.str('DB_MIGRATION_USER',  # noqa: F405
-                 default=DATABASES['default']['USER']),  # noqa: F405
-    NAME=env.str('DB_MIGRATION_NAME',  # noqa: F405
-                 default=DATABASES['default']['NAME']),  # noqa: F405
-    HOST=env.str('DB_MIGRATION_HOST',  # noqa: F405
-                 default=DATABASES['default']['HOST']),  # noqa: F405
-    PORT=env.str('DB_MIGRATION_PORT',  # noqa: F405
-                 default=DATABASES['default']['PORT']),  # noqa: F405
-    OPTIONS=env.json('DB_MIGRATION_OPTIONS',  # noqa: F405
-                     default=DATABASES['default']['OPTIONS']),  # noqa: F405
+    PASSWORD=env.str('DB_MIGRATION_PASS', default=None),  # noqa: F405
+    ENGINE=env.str('DB_MIGRATION_ENGINE', default=None),  # noqa: F405
+    USER=env.str('DB_MIGRATION_USER', default=None),  # noqa: F405
+    NAME=env.str('DB_MIGRATION_NAME', default=None),  # noqa: F405
+    HOST=env.str('DB_MIGRATION_HOST', default=None),  # noqa: F405
+    PORT=env.str('DB_MIGRATION_PORT', default=None),  # noqa: F405
+    OPTIONS=env.json('DB_MIGRATION_OPTIONS', default=None),  # noqa: F405
+
 )
 for override, value in DB_OVERRIDES.items():
-    DATABASES['default'][override] = value  # noqa: F405
+    if value:
+        DATABASES['default'][override] = value  # noqa: F405
 
 # The one thing we don't allow to be set in production, neither from
 # the environment nor from a config file, is DEBUG == True.
